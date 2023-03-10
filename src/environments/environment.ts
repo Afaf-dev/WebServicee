8aeddae711a68bc17a46ac30612bf421f8b0a431
hafsaelmekki <elmekkihafsa@gmail.com>
2023-03-10 12:03:22 +0100

    Initial import

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'projet-20914',
+    appId: '1:565132241343:web:9772f34b5a3b1bc32fd90a',
+    databaseURL: 'https://projet-20914-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'projet-20914.appspot.com',
+    apiKey: 'AIzaSyC_nqRxbXoIv_A507KsvZG3oErb3x-yYPg',
+    authDomain: 'projet-20914.firebaseapp.com',
+    messagingSenderId: '565132241343',
+    measurementId: 'G-EZBMS313VS',
+  },
+  production: false
+};
